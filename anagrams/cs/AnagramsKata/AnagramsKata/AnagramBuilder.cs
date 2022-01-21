9c2985ce11205222420d110be17df22abec25318
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-21 20:07:36 +0100

    Green

--- anagrams/cs/AnagramsKata/AnagramsKata/AnagramBuilder.cs
+++ anagrams/cs/AnagramsKata/AnagramsKata/AnagramBuilder.cs
@@ -4,8 +4,8 @@ public class AnagramBuilder
 {
     public IEnumerable<string> Get(string word)
     {
-        if (word == "AB")
-            return new List<string> { "BA", "AB" };
+        if (word.Length == 2)
+            return new List<string> { word, $"{word[1]}{word[0]}" };
         if (word == "")
             return new List<string>();
         return new List<string> { word };
