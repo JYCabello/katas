b221b61cc8c046b78fa78e36b66261c72abe7f3a
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-21 20:21:07 +0100

    still red

--- anagrams/cs/AnagramsKata/AnagramsKata/AnagramBuilder.cs
+++ anagrams/cs/AnagramsKata/AnagramsKata/AnagramBuilder.cs
@@ -10,6 +10,17 @@ public class AnagramBuilder
         result.Add(word);
         if (word.Length == 2)
             result.Add($"{word[1]}{word[0]}");
+        for (var i = 0; i < word.Length - 1; i++)
+        {
+            var character = word[i];
+            var subPalindromes = Get(WithoutChar(word, i));
+            foreach (var subPalindrome in subPalindromes)
+                result.Add($"{character}{subPalindrome}");
+        }
+
         return result;
+
+        static string WithoutChar(string s, int index) =>
+            "";
     }
 }
