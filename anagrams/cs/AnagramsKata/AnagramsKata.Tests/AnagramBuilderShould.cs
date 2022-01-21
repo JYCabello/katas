48cf3261573a14cd8ba3487e4b5f47408ea53dde
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-21 19:57:26 +0100

    red

--- anagrams/cs/AnagramsKata/AnagramsKata.Tests/AnagramBuilderShould.cs
+++ anagrams/cs/AnagramsKata/AnagramsKata.Tests/AnagramBuilderShould.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using FsCheck;
 using FsCheck.Xunit;
 using Xunit;
 
@@ -7,19 +6,28 @@ namespace AnagramsKata.Tests;
 
 public class AnagramBuilderShould
 {
-    [Fact(DisplayName = "Empty should return an empty collection")]
-    public void Null()
+    [Fact(DisplayName = "return an empty collection for empty")]
+    public void Empty()
     {
         var builder = new AnagramBuilder();
         var result = builder.Get("");
         Assert.Empty(result);
     }
 
-    [Property(DisplayName = "One letter should return a collection with that letter")]
+    [Property(DisplayName = "return a collection with that letter if one letter")]
     public void OneLetter(char letter)
     {
         var builder = new AnagramBuilder();
         var result = builder.Get(letter.ToString());
         Assert.Equal(new List<string> { letter.ToString() }, result);
+    }
+
+    [Fact(DisplayName = "return AB and AB for AB")]
+    public void AB()
+    {
+        var builder = new AnagramBuilder();
+        var result = builder.Get("AB");
+        var expectation = new List<string> { "AB", "BA" };
+        Assert.Equal(expectation, result);
     }
 }
