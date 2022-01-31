7244a1829ac3a33ae401e310b247af417d5b57c2
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-31 17:19:01 +0100

    some massive refactor

--- gameoflife/cs/GameOfLife/GameOfLife/World.cs
+++ gameoflife/cs/GameOfLife/GameOfLife/World.cs
@@ -16,10 +16,7 @@ public class World
                 new[]
                 {
                     false,
-                    WillBeAlive(new[]
-                    {
-                        board[0][0], board[0][1], board[0][2], board[1][0], board[1][2]
-                    }),
+                    WillBeAlive(GetNeighbors(1, 1)),
                     false
                 }
             };
@@ -27,10 +24,41 @@ public class World
         if (board[0].Length == 3)
             return new[]
             {
-                new[] { false, WillBeAlive(new[] { board[0][0], board[0][2] }) }
+                new[] { false, WillBeAlive(GetNeighbors(1, 0)) }
             };
 
         return new[] { new[] { false, false } };
+    }
+
+    public bool[] GetNeighbors(int x, int y)
+    {
+        var neigbors = new List<bool>();
+
+        if (x > 0 && y > 0)
+            neigbors.Add(board[y - 1][x - 1]);
+
+        if (y > 0)
+            neigbors.Add(board[y - 1][x]);
+
+        if (y > 0 && board[y - 1].Length > x + 1)
+            neigbors.Add(board[y - 1][x + 1]);
+
+        if (board[y].Length > x + 1)
+            neigbors.Add(board[y][x + 1]);
+
+        if (board.Length > y + 1 && board[y].Length > x + 1)
+            neigbors.Add(board[y + 1][x + 1]);
+
+        if (board.Length > y + 1)
+        {
+            neigbors.Add(board[y + 1][x]);
+            neigbors.Add(board[y + 1][x - 1]);
+        }
+
+        if (x > 0)
+            neigbors.Add(board[y][x - 1]);
+
+        return neigbors.ToArray();
     }
 
     public bool WillBeAlive(bool[] neighbors)
