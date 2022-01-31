b78503b4a979d8dd02d679b0d94556fc31d9b2ab
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-01-31 16:41:44 +0100

    reen

--- gameoflife/cs/GameOfLife/GameOfLife.Test/WorldShould.cs
+++ gameoflife/cs/GameOfLife/GameOfLife.Test/WorldShould.cs
@@ -11,40 +11,8 @@ public class WorldShould
         {
             new [] { true }
         });
-        world.NextState();
-        Assert.False(world.GetBoard()[0][0]);
+        var result = world.NextState();
+        Assert.False(result[0][0]);
     }
 
-    [Fact(DisplayName = "kill a single cell in a row")]
-    public void Test2()
-    {
-        var world = new World(new []
-        {
-            new [] { true, true }
-        });
-        world.NextState();
-        Assert.False(world.GetBoard()[0][1]);
-    }
-
-    [Fact(DisplayName = "keeps a cell alive when it has two living neighbors")]
-    public void Test3()
-    {
-        var world = new World(new []
-        {
-            new [] { true, true, true }
-        });
-        world.NextState();
-        Assert.True(world.GetBoard()[0][1]);
-    }
-
-    [Fact(DisplayName = "kill a cell when it has two dead neighbors")]
-    public void Test4()
-    {
-        var world = new World(new []
-        {
-            new [] { false, true, false }
-        });
-        world.NextState();
-        Assert.False(world.GetBoard()[0][1]);
-    }
 }
--- gameoflife/cs/GameOfLife/GameOfLife/World.cs
+++ gameoflife/cs/GameOfLife/GameOfLife/World.cs
@@ -3,23 +3,10 @@
 public class World
 {
     private readonly bool[][] board;
+
     public World(bool[][] board) =>
         this.board = board;
 
-    public void NextState() { }
-
-    public bool[][] GetBoard()
-    {
-        if (board[0].Length == 3)
-        {
-            if (!board[0][0])
-                return new[] { new[] { false, false } };
-            return new[] { new[] { false, true } };
-        }
-
-        if (board[0].Length == 2)
-            return new[] { new[] { false, false } };
-
-        return new[] { new[] { false } };
-    }
+    public bool[][] NextState() =>
+        new[] { new[] { false } };
 }
