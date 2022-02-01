2b126f5e9e5718d3ef13ad936d9aac3597c75b5c
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-01 10:34:59 +0100

    green

--- gameoflife/cs/GameOfLife/GameOfLife/Board.cs
+++ gameoflife/cs/GameOfLife/GameOfLife/Board.cs
@@ -9,17 +9,22 @@ public class Board
 
     public bool[] GetNeighbors(int y, int x)
     {
+        var height = board.GetLength(0);
         var width = board.GetLength(1);
         var neigbors = new List<bool>();
 
         var isOnRightBorder = width == x + 1;
         var isOnLeftBorder = x == 0;
+        var isOnBottomBorder = height == y + 1;
 
         if (!isOnRightBorder)
             neigbors.Add(board[y, x + 1]);
 
         if (!isOnLeftBorder)
             neigbors.Add(board[y, x - 1]);
+
+        if (!isOnBottomBorder)
+            neigbors.Add(board[y + 1, x]);
 
         return neigbors.ToArray();
     }
