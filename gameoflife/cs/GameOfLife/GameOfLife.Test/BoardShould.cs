e1300c1ce32dfd7cb7433db2074d1ea5b4cc5bda
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-01 10:31:13 +0100

    red

--- gameoflife/cs/GameOfLife/GameOfLife.Test/BoardShould.cs
+++ gameoflife/cs/GameOfLife/GameOfLife.Test/BoardShould.cs
@@ -11,4 +11,22 @@ public class BoardShould
         var result = board.GetNeighbors(0, 0);
         Assert.Empty(result);
     }
+
+    [Fact(DisplayName = "gets one neighbor on the right")]
+    public void Test02()
+    {
+        var board = new Board(new[,] { { true, false } });
+        var result = board.GetNeighbors(0, 0);
+        Assert.Single(result);
+        Assert.False(result[0]);
+    }
+
+    [Fact(DisplayName = "gets one neighbor on the left")]
+    public void Test03()
+    {
+        var board = new Board(new[,] { { true, false } });
+        var result = board.GetNeighbors(0, 1);
+        Assert.Single(result);
+        Assert.True(result[0]);
+    }
 }
--- gameoflife/cs/GameOfLife/GameOfLife/Board.cs
+++ gameoflife/cs/GameOfLife/GameOfLife/Board.cs
@@ -7,10 +7,15 @@ public class Board
     public Board(bool[,] board) =>
         this.board = board;
 
-    public bool[] GetNeighbors(int x, int y)
+    public bool[] GetNeighbors(int y, int x)
     {
+        var width = board.GetLength(1);
         var neigbors = new List<bool>();
 
+        var isOnRightBorder = width == x + 1;
+
+        if (!isOnRightBorder)
+            neigbors.Add(board[y, x + 1]);
 
 
         return neigbors.ToArray();
