e5690604b8c08fbb4c366efc54863d909ca96a69
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-07 15:13:10 +0100

    testing console output

--- fakes/Fakes/BalanceTracker.Tests/AcceptanceTest.cs
+++ fakes/Fakes/BalanceTracker.Tests/AcceptanceTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using Xunit;
 
 namespace BalanceTracker.Tests;
@@ -7,12 +9,15 @@ public class AcceptanceTest
     [Fact(DisplayName = "Should accept amounts")]
     public void Test1()
     {
+        using var stringWriter = new StringWriter();
+        Console.SetOut(stringWriter);
         var userInterface = new UserInterface();
-        var balance = new Balance(userInterface); // shoes
+        var balance = new Balance(userInterface);
         balance.Append(1);
         balance.Append(2);
         balance.Append(3);
         balance.Append(-2);
         balance.Print();
+        Assert.Equal($"4{Environment.NewLine}", stringWriter.ToString());
     }
 }
