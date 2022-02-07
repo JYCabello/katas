40219f061cb2dfa42c99487a84ef5ef1eea3aba9
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-07 15:08:43 +0100

    console output

--- fakes/Fakes/BalanceTracker/Program.cs
+++ fakes/Fakes/BalanceTracker/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using BalanceTracker;
+
+var balance = new Balance(new UserInterface());
+balance.Append(8);
+balance.Print();
