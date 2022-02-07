c5fe3895d14d22c26421edb7b6ed527ecfa73410
Yeray Cabello <j.cabello.ramos@gmail.com>
2022-02-07 14:49:55 +0100

    green

--- fakes/Fakes/BalanceTracker.Tests/BalanceShould.cs
+++ fakes/Fakes/BalanceTracker.Tests/BalanceShould.cs
@@ -13,6 +13,18 @@ public class BalanceShould
         balance.Print();
         Assert.Equal("5", mokUi.Output);
     }
+
+    [Fact(DisplayName = "Should display a certain number as balance after more operations")]
+    public void Test2()
+    {
+        var mokUi = new MockUserInterface();
+        var balance = new Balance(mokUi);
+        balance.Append(5);
+        balance.Append(7);
+        balance.Append(-3);
+        balance.Print();
+        Assert.Equal("9", mokUi.Output);
+    }
 }
 
 public class MockUserInterface : IUserInterface
